a978fc66a81b021f215774ec09d01065aca32e2e
Mario <lm0013022019@unab.edu.sv>
2022-03-12 20:08:19 -0600

    first commit

---
+++ StudentCRUD/StudentCRUD/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StudentCRUD.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        public DateTime? RecordCreatedOn { get; set; }
+
+    }
+}
